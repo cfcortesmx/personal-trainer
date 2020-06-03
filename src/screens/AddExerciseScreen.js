b179b6eba8925ef9fb572615ae4aa635d1d3f3cd
Carlos Flores <cfcortes75@gmail.com>
2020-06-03 06:40:58 -0500

    Added react router

---
+++ src/screens/AddExerciseScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const AddExerciseScreen = () => {
+  return <Text>Add Exercise Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AddExerciseScreen;
