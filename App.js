d207e5842eff9bce53727274fe17422e23e85b81
cfcortesmx <cfcortes@ucol.mx>
2020-06-05 06:10:11 -0500

    Added ui-kitten

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
-import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import {AppNavigator} from './src/components/navigation.component';
 
-import AddExerciseScreen from './src/screens/AddExerciseScreen';
+import {default as theme} from './config/custom-theme.json';
 
-const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="AddExercise">
-        <Stack.Screen
-          name="AddExercise"
-          component={AddExerciseScreen}
-          options={{title: 'Agregar Ejercicio'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-export default App;
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={{...eva.dark, ...theme}}>
+      <AppNavigator />
+    </ApplicationProvider>
+  </>
+);
--- src/screens/AddExerciseScreen.js
+++ src/screens/AddExerciseScreen.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import {Button, Divider, Layout, TopNavigation} from '@ui-kitten/components';
 
-const AddExerciseScreen = () => {
-  return <Text>Add Exercise Screen</Text>;
+const FacebookIcon = (props) => <Icon name="facebook" {...props} />;
+
+const AddExerciseScreen = ({navigation}) => {
+  const navigateDetails = () => {
+    navigation.navigate('Details');
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <TopNavigation title="MyApp" alignment="center" />
+      <Divider />
+      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Button onPress={navigateDetails}>OPEN DETAILS</Button>
+      </Layout>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
